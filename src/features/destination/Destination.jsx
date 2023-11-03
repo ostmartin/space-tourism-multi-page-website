c4f1a2a04a48a03a2f4f6d02f5e40de58bb9f38a
Arseniy2403 <arseniyermoshkin2403@gmail.com>
2023-11-03 12:10:58 +0200

    dynamic content for destination

--- src/features/destination/Destination.jsx
+++ src/features/destination/Destination.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Tab, Heading, SubHeading, BodyText } from '../../ui';
 
-import Moon from './images/image-moon.png';
-
 const Destination = () => {
+    const [currentPlanet, setCurrentPlanet] = useState({
+      "name": "Moon",
+      "images": {
+        "png": "src/assets/images/destination/image-moon.png",
+        "webp": "src/assets/images/destination/image-moon.webp"
+      },
+      "description": "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
+      "distance": "384,400 km",
+      "travel": "3 days"
+    });
+
+    const getNewPlanet = () => {
+        fetch('src/data.json')
+            .then(r => r.json())
+            .then(r => r.destinations[0])
+            .then(setCurrentPlanet)
+    }
+
+    useEffect(() => {
+        getNewPlanet()
+    }, [])
+
+    const {name, description, distance, travel, images} = currentPlanet;
+
     return (
         <div className='px-6 tablet:px-8 desktop:px-40 pt-10 desktop:pt-20 max-w-[1440px] xxl:mx-auto'>
             <div className='heading-5 desktop:text-h28 mobile:text-center text-[#fff!important] tablet:text-left mobile:text-[16px] tablet:text-[20px]'>
@@ -13,7 +35,7 @@ const Destination = () => {
             </div>
             <section className='flex flex-col pt-16 justify-between desktop:flex-row items-center'>
                 <div className='w-4/6 desktop:w-[55%]'>
-                    <img className='m-auto' src={Moon} alt="Moon" />
+                    <img className='m-auto' src={images["png"]} alt={name} />
                 </div>
                 <div className='h-full w-5/6 desktop:max-w-[50%] desktop:w-[35%] py-16'>
                     <ul className='flex w-full gap-8 mb-4 justify-center desktop:justify-start'>
@@ -23,16 +45,16 @@ const Destination = () => {
                         <Tab text={'Titan'}/>
                     </ul>
                     <div className='text-center desktop:text-left'>
-                        <Heading text={'Moon'} level={'2'}/>
-                        <BodyText addClass={'pb-10 pe-4'} text={'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.'}/>
+                        <Heading text={name} level={'2'}/>
+                        <BodyText addClass={'pb-10 pe-4'} text={description}/>
                         <div className='flex flex-row mobile:flex-col mobile:gap-10 justify-around desktop:justify-between pt-4 border-t-[1px] border-solid border-[#383b4b]'>
                             <div>
                                 <SubHeading text={'avg. distance'} level={'2'}/>
-                                <SubHeading text={'384,400 km'} level={'1'}/>
+                                <SubHeading text={distance} level={'1'}/>
                             </div>
                             <div>
                                 <SubHeading text={'est. travel time'} level={'2'}/>
-                                <SubHeading text={'3 days'} level={'1'}/>                                
+                                <SubHeading text={travel} level={'1'}/>                                
                             </div>
                         </div>
                     </div>
